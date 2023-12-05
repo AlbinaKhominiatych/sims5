d3fe112ab4d067103006a7ed4f937ba3c6be629f
khominiatych <hominyatichalbina@gmail.com>
2023-12-05 16:51:29 +0200

    додала атрибути

---
+++ main.py
@@ -0,0 +1,14 @@
+#створення симуляції життя
+class Human:
+    def __init__(self,
+                 name = "Human",
+                 job = None,
+                 home = None,
+                 car = None):
+        self.name = name
+        self.money = 50_000
+        self.gladness = 50 #радість
+        self.satiety = 50 #харчування
+        self.job = job
+        self.car = car
+        self.home = home
